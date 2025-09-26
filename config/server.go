a6b6fbc838c51989db7d4318cbde7b7b59cf77ed
allopopot <allopopot@outlook.com>
2025-09-26 22:46:18 +0530

    new cors handler to allow all origins for testing

--- config/server.go
+++ config/server.go
@@ -9,5 +9,3 @@ var SERVER_HOST string = "0.0.0.0"
 var SERVER_PORT string = utility.ParseEnv("SERVER_PORT", true, "4000")
 
 var SERVER_URI = fmt.Sprintf("%s:%s", SERVER_HOST, SERVER_PORT)
-
-var CORS_ORIGINS = []string{"http://localhost:5173", "http://localhost:4173"}
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"allopopot-interconnect-service/dbcontext"
 	"allopopot-interconnect-service/routes"
 	"allopopot-interconnect-service/service/emailqueue"
-	"strings"
 
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
@@ -18,9 +17,15 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     strings.Join(config.CORS_ORIGINS, ","),
+		AllowOriginsFunc: func(origin string) bool {
+			return true
+		},
 		AllowCredentials: true,
 	}))
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     strings.Join(config.CORS_ORIGINS, ","),
+	// 	AllowCredentials: true,
+	// }))
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"message": "pong"})
