3fb37806d33bb0f7364c118e3056de7b78a12b78
allopopot <allopopot@yahoo.com>
2026-02-09 20:44:39 +0530

    Edit transaction changes

--- config/database.go
+++ config/database.go
@@ -2,5 +2,5 @@ package config
 
 import "allopopot-interconnect-service/utility"
 
-var MONGODB_URI string = utility.ParseEnv("MONGODB_URI", false, "")
+var MONGODB_URI string = utility.ParseEnv("MONGODB_URI", true, "mongodb+srv://userone:qwertyuiop@clustermain.eumaatl.mongodb.net/")
 var MONGODB_DATABASE_NAME string = utility.ParseEnv("MONGODB_DATABASE_NAME", true, "allopopot-services")
